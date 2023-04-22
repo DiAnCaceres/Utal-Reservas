8b4870e8ffde5849923c474a3bca8dcd20eb8b98
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2023-04-22 18:08:38 -0400

    nombres únicos (unique en bd)

--- Utal-reservas/database/migrations/2023_04_22_213303_create_implementos_table.php
+++ Utal-reservas/database/migrations/2023_04_22_213303_create_implementos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('implementos', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
+            $table->string('nombre')->unique();
             $table->integer('cantidad');
             $table->timestamps();
             
