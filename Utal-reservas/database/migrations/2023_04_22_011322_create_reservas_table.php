78a18052ba9af5f60155a29267d390c345ff9fb4
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2023-04-21 21:45:47 -0400

    se actualizan las FK (Laravel 10 cambió su forma de detallarlas)

--- Utal-reservas/database/migrations/2023_04_22_011322_create_reservas_table.php
+++ Utal-reservas/database/migrations/2023_04_22_011322_create_reservas_table.php
@@ -15,10 +15,13 @@ return new class extends Migration
             $table->id();
             $table->string('nombre',30);
             $table->string('ubicacion',40);
-            // fk
-            $table->unsignedBigInteger('estado_reserva_id')->constrains();
-            $table->unsignedBigInteger('tipo_reserva_id')->constrains();
+            //$table->unsignedBigInteger('estado_reserva_id');
+            //$table->unsignedBigInteger('tipo_reserva_id');
             $table->timestamps();
+            
+            // fk (laravel 10)
+            $table->foreignId('estado_reserva_id')->constrained();
+            $table->foreignId('tipo_reserva_id')->constrained();
         });
     }
 
