6fea105d156f44c9a8c7865b5e77173a25851491
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2023-04-22 17:33:48 -0400

    se crean las nuevas migraciones sist. reservas

--- Utal-reservas/database/migrations/2023_04_22_213252_create_sala_gimnasios_table.php
+++ Utal-reservas/database/migrations/2023_04_22_213252_create_sala_gimnasios_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tipo_reservas', function (Blueprint $table) {
+        Schema::create('sala_gimnasios', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre_tipo');
             $table->timestamps();
         });
     }
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tipo_reservas');
+        Schema::dropIfExists('sala_gimnasios');
     }
 };
