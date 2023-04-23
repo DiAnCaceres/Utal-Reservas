322b8d95f4ba49789821ce9e1419fa7b97789082
Diego <diegoxtapia@gmail.com>
2023-04-23 00:50:59 -0400

    Vistas de registro de servicios
    Se añadieron vistas provisionales para registrar salas de estudio, gimnasio, cancha e implementos. Tambien se unieron mediante botones

---
+++ Utal-reservas/resources/views/registro/registrar_implemento.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Registar Implemento')
+@section('content')
+    <h1> aqui vamos a registrar los implementos </h1>
+    <button type="button" onclick="window.location='{{ route('login') }}'">Ir al Login</button>
+    <button type="button" onclick="window.location='{{ route('registro_sala_estudio') }}'">Ir al Registro</button>
+    <div>
+    <button type="button" onclick="window.location='{{ route('registro_sala_estudio') }}'">Salas de estudio</button>
+    <button type="button" onclick="window.location='{{ route('registro_sala_gimnasio') }}'">Salas del gimnasio</button>
+    <button type="button" onclick="window.location='{{ route('registro_cancha') }}'">Canchas</button>
+    <button type="button" onclick="window.location='{{ route('registro_implemento') }}'">Implementos</button>
+    </div>
+
+
+@endsection 
---
+++ Utal-reservas/resources/views/registro/registrar_sala_gimnasio.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Registar Sala del Gimnasio')
+@section('content')
+<h1> aqui vamos a registrar las salas del gimnasio </h1>
+    <button type="button" onclick="window.location='{{ route('login') }}'">Ir al Login</button>
+    <button type="button" onclick="window.location='{{ route('registro_sala_estudio') }}'">Ir al Registro</button>
+    <div>
+    <button type="button" onclick="window.location='{{ route('registro_sala_estudio') }}'">Salas de estudio</button>
+    <button type="button" onclick="window.location='{{ route('registro_sala_gimnasio') }}'">Salas del gimnasio</button>
+    <button type="button" onclick="window.location='{{ route('registro_cancha') }}'">Canchas</button>
+    <button type="button" onclick="window.location='{{ route('registro_implemento') }}'">Implementos</button>
+    </div>
+
+    
+@endsection 
