b212c86ec828d6d805f775d130a876ccc37bbd68
sctavo <gsgsnchz09@gmail.com>
2023-04-22 21:42:05 -0400

    Aplicacion de plantilla de login
    Se aplico la plantilla que se propuso para el inicio de sesión cambiando algunos aspectos visuales y logicos (se quitó la parte de registrar por que no tenia sentido), se agregó también una vista en cambio de registrar *necesitas ayuda*, generando rutas entre login y ayuda.

--- Utal-reservas/routes/web.php
+++ Utal-reservas/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
+
+// Route::get('/', HomeController::class, 'login');
+
+Route::get('login', [LoginController::class, 'index'])->name('login');
+
+Route::get('/ayuda', function () {
+    return view('ayuda');
+})->name('ayuda');
+
