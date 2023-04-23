f56a61eccae8ab1fd177b51667363e61dcb711c5
JavierVF <jvillarroel20@alumnos.utalca.cl>
2023-04-23 17:05:09 -0400

    Union Trabajo Tapia y Javier
    Se unieron los trabajos realizados por los dos integrantes esto consiste en el apartado de registros que consta con los servisios de salas, gimnacio, cancha y implementos, estos estan realizados con cajas de texto pero se espera retroalimentacion para proximas modificaciones RU04,RU10,RU16,RU22

--- Utal-reservas/resources/views/registro/registrar_implemento.blade.php
+++ Utal-reservas/resources/views/registro/registrar_implemento.blade.php
@@ -2,16 +2,31 @@
 
 @section('title', 'Registar Implemento')
 @section('content')
-<div class="row">
-        <div class="botonera">
-            <button type="button" class="btn btn-default col-xs-4 boton_servicios" onclick="window.location='{{ route('registro_sala_estudio') }}'">Salas de estudio</button>
-            <button type="button" class="btn btn-default col-xs-4 boton_servicios" onclick="window.location='{{ route('registro_sala_gimnasio') }}'"> Salas del gimnasio</button>
-            <button type="button" class="btn btn-default col-xs-4 boton_servicios" onclick="window.location='{{ route('registro_cancha') }}'">Canchas</button>
-            <button type="button" class="btn btn-default col-xs-4 boton_activo">Implementos</button>
-        </div>
+
+<div class="botonera">
+    <button type="button" class="btn btn-default col-xs-4 boton_servicios" onclick="window.location='{{ route('registro_sala_estudio') }}'">Salas de estudio</button>
+    <button type="button" class="btn btn-default col-xs-4 boton_servicios" onclick="window.location='{{ route('registro_sala_gimnasio') }}'"> Salas del gimnasio</button>
+    <button type="button" class="btn btn-default col-xs-4 boton_servicios" onclick="window.location='{{ route('registro_cancha') }}'">Canchas</button>
+    <button type="button" class="btn btn-default col-xs-4 boton_activo">Implementos</button>
+</div>
+
+<div class="separacion">
+</div>
+
+<div class="contenedor">
+
+    <div class="box_registro_ligteblue">
+        <h1> Registrar Implemento</h1>
+        <form action="">
+
+            <input type="text" placeholder="Nombre">
+            <input type="text" placeholder="Capacidad">
+            <input type="text" placeholder="Ubicacion">
+            <input type="text" placeholder="Descripcion">
+            <input type="text" placeholder="Datos extra">
+            <button class="btnEntrar">Entrar<i class="ri-arrow-right-line"></i></button>
+        </form>
     </div>
-   
-     <h1> Aquí vamos a registrar los implementos</h1>
-   
+</div>
 
 @endsection 
--- Utal-reservas/resources/views/registro/registrar_sala_gimnasio.blade.php
+++ Utal-reservas/resources/views/registro/registrar_sala_gimnasio.blade.php
@@ -2,17 +2,29 @@
 
 @section('title', 'Registar Sala del Gimnasio')
 @section('content')
-<div class="row">
-        <div class="botonera">
-            <button type="button" class="btn btn-default col-xs-4 boton_servicios" onclick="window.location='{{ route('registro_sala_estudio') }}'">Salas de estudio</button>
-            <button type="button" class="btn btn-default col-xs-4 boton_activo"> Salas del gimnasio</button>
-            <button type="button" class="btn btn-default col-xs-4 boton_servicios" onclick="window.location='{{ route('registro_cancha') }}'">Canchas</button>
-            <button type="button" class="btn btn-default col-xs-4 boton_servicios" onclick="window.location='{{ route('registro_implemento') }}'">Implementos</button>
-        </div>
-    </div>
-   
-     <h1> Aquí vamos a registrar las salas del gimnasio </h1>
-   
 
-    
+<div class="botonera">
+    <button type="button" class="btn btn-default col-xs-4 boton_servicios" onclick="window.location='{{ route('registro_sala_estudio') }}'">Salas de estudio</button>
+    <button type="button" class="btn btn-default col-xs-4 boton_activo"> Salas del gimnasio</button>
+    <button type="button" class="btn btn-default col-xs-4 boton_servicios" onclick="window.location='{{ route('registro_cancha') }}'">Canchas</button>
+    <button type="button" class="btn btn-default col-xs-4 boton_servicios" onclick="window.location='{{ route('registro_implemento') }}'">Implementos</button>
+</div>
+
+<div class="separacion">
+</div>
+
+<div class="contenedor">
+
+    <div class="box_registro_ligteblue">
+        <h1> Registrar las salas de Gimnasio</h1>
+        <form action="">
+
+            <input type="text" placeholder="Nombre">
+            <input type="text" placeholder="Capacidad">
+            <input type="text" placeholder="Ubicacion">
+            <button class="btnEntrar">Entrar<i class="ri-arrow-right-line"></i></button>
+        </form>
+    </div>
+</div>
+
 @endsection 
