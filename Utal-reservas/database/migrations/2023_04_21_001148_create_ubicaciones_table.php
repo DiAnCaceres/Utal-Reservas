ad88c3a0eca7505cc0ca42a5962aff83bb5312ba
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2023-04-23 20:13:55 -0400

    ubicaciones es una tabla, reserva tiene su fk

--- Utal-reservas/database/migrations/2023_04_21_001148_create_ubicaciones_table.php
+++ Utal-reservas/database/migrations/2023_04_21_001148_create_ubicaciones_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('ubicaciones', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre_ubicacion',50);
             $table->timestamps();
         });
     }
