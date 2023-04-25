1b9c6562b8555ad6d028f8c1b271522efe4dd3f5
TeaTime <mushashourrea@gmail.com>
2023-04-24 20:36:11 -0400

    Instalación de dependencias Login
    se actualizan los archivos para construir el sistema de Login

--- Utal-reservas/routes/web.php
+++ Utal-reservas/routes/web.php
@@ -44,3 +44,6 @@ Route::post("/registrar-sala-estudio",[SalaEstudioController::class,"store"])->n
 Route::post("/registrar-cancha",[CanchaController::class,"store"])->name("registro_cancha.store");
 Route::post("/registrar-sala-gimnasio",[SalaGimnasioController::class,"store"])->name("registro_sala_gimnasio.store");
 Route::post("/registrar-implemento",[ImplementoController::class,"store"])->name("registro_implemento.store");
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
