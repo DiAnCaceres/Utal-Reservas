8ccd4e565f41fc013c21144a58f55850147c41e7
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2023-04-27 22:23:41 -0400

    migraciones instancia, estado_instancia e historial + seeders

---
+++ Utal-reservas/database/migrations/2023_04_28_015436_create_historial_reservas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('historial_reservas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('instancia_reserva_fecha_reserva');
+            $table->foreignId('instancia_reserva_usuario_id');
+            $table->foreignId('instancia_reserva_bloque_id');
+            $table->foreignId('estado_instancia_reserva_id');
+            $table->date('fecha');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('historial_reservas');
+    }
+};
---
+++ Utal-reservas/database/seeders/InstanciaReservas.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class InstanciaReservas extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+    }
+}
