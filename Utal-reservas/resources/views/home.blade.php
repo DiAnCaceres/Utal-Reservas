90d5222806056c75033609b7d29c3ef09572cd6a
sctavo <gsgsnchz09@gmail.com>
2023-04-28 22:07:09 -0400

    Se crean botones en la pantalla home
    4 botones a las distintas rutas trabajadas de frontend esta semana (3)

--- Utal-reservas/resources/views/home.blade.php
+++ Utal-reservas/resources/views/home.blade.php
@@ -10,5 +10,13 @@
 @section('content')
     <h1>Bienvenido a la página principal</h1>
     <button type="button" onclick="window.location='{{ route('login') }}'">Ir al Login</button>
+    
+    
+    ->Botones provisionales que despues se borran (hace mas facil el testeo)->
+    <button type="button" onclick="window.location='{{ route('reservar_cancha') }}'">Reservar cancha</button>
+    <button type="button" onclick="window.location='{{ route('reservar_sala_estudio') }}'">Reservar sala estudio</button>
+    <button type="button" onclick="window.location='{{ route('reservar_sala_gimnasio') }}'">Reservar sala gimnasio</button>
+    <button type="button" onclick="window.location='{{ route('reservar_implemento') }}'">Reservar implemento</button>
+
 
 @endsection
