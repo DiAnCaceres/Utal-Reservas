b561937ee26573e8ed06ba79777fc1e7d34bc055
JavierVF <jvillarroel20@alumnos.utalca.cl>
2023-04-30 23:32:43 -0400

    Desarrollo vistas registrar
    se implemento la funcionalidad de registrar para frondend este aun le falta trabajo para que registre, sus imput son fecha y bloques para que arroje las salas que estaran habiles

--- Utal-reservas/resources/views/reservar/implemento.blade.php
+++ Utal-reservas/resources/views/reservar/implemento.blade.php
@@ -17,34 +17,30 @@
     <button type="button" class="btn btn-default col-xs-4 boton_activo">Implementos</button>
 </div>
 
-<div class="separacion">
+<div class="separacion">                        <!-- Contenedor para un separador, esto con el fin de que quede en el centro el boloque celeste  -->
 </div>
 
-<div class="contenedor">
-    <div class="box_registro_ligteblue">
+<div class="contenedorReserva">                      <!-- Contenedor general  -->
+    <div class="box_reserva_ligteblue">             <!-- Caja celeste que engloba a los datos  -->
+
         <h1><b> Reservar implementos </b></h1>
         <form action="{{route('reservar_implemento')}}" method="POST">
-                @csrf
+            @csrf
+            <input class="form-control" type="fecha-local" placeholder="Seleccionar fecha">
 
-                <label for='bloques' style="margin-right: 150px;">Bloques:</label>
-                <select name=bloques" id="bloques">
-                @foreach($bloquesDisponibles as $bloque)
-                    <option name="bloque" value="{{ $bloque }}">{{ $bloque->hora_inicio }} - {{ $bloque->hora_fin }}</option>
-                @endforeach
-                </select>
+            <label for='bloques' style="margin-right: 200px;">Bloques:</label>
+            <select name=bloques" id="bloques">
+            @foreach($bloquesDisponibles as $bloque)
+                <option name="bloque" value="{{ $bloque }}">{{ $bloque->hora_inicio }} - {{ $bloque->hora_fin }}</option>
+            @endforeach
+            </select>
 
-                
-                <input class="form-control" type="fecha-local" placeholder="Seleccionar fecha">
-
-                
-                <button class="button-register">Reservar<i class="ri-arrow-right-line"></i></button>
-
-            </form>      
-
-
+            <button type="button" onclick="window.location='{{ route('reservar_implementos_disponibles') }}'">Buscar canchas disponibles</button>
+        </form>      
     </div>
         
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
     <script>flatpickr("input[type=fecha-local]",{})</script>
+
 </div>
 @endsection
