a33d8416be5f447500f99a26b3333f38eae355e2
Alonso <aorostica20@alumnos.utalca.cl>
2023-05-01 15:49:46 -0400

    Controladores de cancha e implementos
    Se configuraron los controladores para reservar esos servicios. Falta corregir unos detalles con las vistas

--- Utal-reservas/app/Http/Controllers/Reservas/CanchaController.php
+++ Utal-reservas/app/Http/Controllers/Reservas/CanchaController.php
@@ -8,7 +8,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class CanchaController extends Controller
-{
+{   
+    // Atributos para la reserva
+    private $id_bloque;
+    private $fecha_reserva;
+    private $id_usuario;
+    private $id_cancha;
+
     //
     public function store(CanchaRequest $request){
         $sql=true;
@@ -38,5 +44,54 @@ class CanchaController extends Controller
         } catch (\Throwable $th) {
             return back()->with('error', '¡Hubo un error al guardar el registro!');
         }
+    }
+
+    public function reservar(CanchaRequest $request) {
+
+        try {
+            
+            //OBTENGO EL ID DEL BLOQUE QUE SE SELECIONÓ
+            $this->id_bloque=$request->bloque->id;
+
+            //OBTENER EL ESTUDIANTE
+            $this->id_usuario=$request->user()->id;
+
+            //OBTENER FECHA DE LA RESERVA
+            $this->fecha_reserva=$request->fecha;
+
+            //OBTENER ID DE LA RESERVA
+            $this->id_cancha = $request->cancha->id;
+
+            DB::table("instancia_reservas")->insert([
+                "bloque_id" => $this->id_bloque, 
+                "user_id" => $this->id_usuario,
+                "fecha_reserva" => $this->fecha_reserva,
+                "reserva_id" => $this->id_cancha, 
+            ]);
+
+            $estado_instancia_reserva = DB::table("estado_instancia_reservas")->where('nombre_estado', 'reservado')->first();
+
+            $id_estado_instancia = $estado_instancia_reserva->id;
+
+            DB::table("historial_reservas")->insert([
+                "instancia_reserva_fecha_reserva"=>$this->fecha_reserva,
+                "instancia_reserva_user_id"=>$this->id_usuario,
+                "instancia_reserva_bloque_id"=>$this->id_bloque,
+                "estado_instancia_reserva_id"=>$id_estado_instancia,
+                "fecha"=>date('Y-m-d')      //ESTA ES EL DÍA EN QUE SER RESERVÓ
+            ]);
+
+            return back()->with('success', "Reserva de cancha registrada correctamente");
+        } catch (\Throwable $th) {
+            //throw $th;
+            return back()->with('error', '¡Hubo un error al reservar!');
+        }
+
+    }
+
+    public function canchas_disponibles(CanchaRequest $request){
+
+        // $this->reservar($request);
+
     }
 }
--- Utal-reservas/app/Http/Controllers/Reservas/ImplementoController.php
+++ Utal-reservas/app/Http/Controllers/Reservas/ImplementoController.php
@@ -10,6 +10,12 @@ use App\Models\Ubicacion;
 
 class ImplementoController extends Controller
 {
+    // atributos para la reserva
+    private $id_bloque;
+    private $fecha_reserva;
+    private $id_usuario;
+    private $id_cancha;
+
     //
     public function store(ImplementoRequest $request){
         $sql=true;
@@ -45,5 +51,50 @@ class ImplementoController extends Controller
         $ubicacionesDeportivas = Ubicacion::where('categoria', 'deportivo')->whereNotIn('nombre_ubicacion',['aire libre'])->get();
         
         return view('registro.registrar_implemento', compact('ubicacionesDeportivas'));
+    }
+
+    public function reservar(ImplementoRequest $request){
+
+        
+        try {
+            
+            //OBTENGO EL ID DEL BLOQUE QUE SE SELECIONÓ
+            $this->id_bloque=$request->bloque->id;
+
+            //OBTENER EL ESTUDIANTE
+            $this->id_usuario=$request->user()->id;
+
+            //OBTENER FECHA DE LA RESERVA
+            $this->fecha_reserva=$request->fecha;
+
+            //OBTENER ID DE LA RESERVA
+            $this->id_cancha = $request->cancha->id;
+
+            DB::table("instancia_reservas")->insert([
+                "bloque_id" => $this->id_bloque, 
+                "user_id" => $this->id_usuario,
+                "fecha_reserva" => $this->fecha_reserva,
+                "reserva_id" => $this->id_cancha, 
+            ]);
+
+            $estado_instancia_reserva = DB::table("estado_instancia_reservas")->where('nombre_estado', 'reservado')->first();
+
+            $id_estado_instancia = $estado_instancia_reserva->id;
+
+            DB::table("historial_reservas")->insert([
+                "instancia_reserva_fecha_reserva"=>$this->fecha_reserva,
+                "instancia_reserva_user_id"=>$this->id_usuario,
+                "instancia_reserva_bloque_id"=>$this->id_bloque,
+                "estado_instancia_reserva_id"=>$id_estado_instancia,
+                "fecha"=>date('Y-m-d')      //ESTA ES EL DÍA EN QUE SER RESERVÓ
+            ]);
+
+            return back()->with('success', "Reserva de cancha registrada correctamente");
+            
+        } catch (\Throwable $th) {
+            //throw $th;
+            return back()->with('error', '¡Hubo un error al reservar!');
+        }
+
     }
 }
