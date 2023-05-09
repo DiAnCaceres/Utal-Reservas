15753e99b7e2b14610ed4bd9e674b9a468f24329
Bastián Núñez <bastinuez@hotmail.com>
2023-05-09 17:43:35 -0400

    Penalizacion basico

--- Utal-reservas/app/Providers/AppServiceProvider.php
+++ Utal-reservas/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Helpers\PenalizacionHelper;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        $this->app->booted(function () {
+            PenalizacionHelper::verificarnoAsiste();
+        });
     }
 }
