28df1625f2dc856ef37f738f72e9902fdead6a08
JavierVF <jvillarroel20@alumnos.utalca.cl>
2023-05-09 03:35:09 -0400

    Avance de requisito
    ordene la paguina agregue rut y tabla pero aun no le ingreso los datos.

--- Utal-reservas/resources/views/Cancha/entregar.blade.php
+++ Utal-reservas/resources/views/Cancha/entregar.blade.php
@@ -19,29 +19,58 @@
         <button type="button" class="btn btn-default col-xs-4 boton_activo">Implementos</button>
          -->
     </div>
-
-    <h1> Entregar cancha</h1>
-
-
-    <form action="{{route('post_cancha_entregar')}}" method="POST">
-        @csrf
-        <button type="submit">Buscar reservas del usuario</button>
-    </form>
-
-    <div id="div_resultados">
-        <h1> Resultados de busqueda </h1>
-        <form action="{{route('post_cancha_entregar_resultados')}}" method="POST">
+    
+    <br><br>
+    <div class="box_entregar_ligteblue">     
+        <h1><b> Buscar reserva para entregar cancha: </b></h1>
+    
+        <form action="{{route('post_cancha_entregar')}}" method="POST">
             @csrf
-
-            @if($mostrarResultados == true && $resultados != "")
-                <p> {{$resultados}}</p>
-                <button type="submit">Entregar</button>
-            @else
-                <p>No se encontraron resultados.</p>
-            @endif
-
+            <input type="text" placeholder="Rut: 12.345.678-9" name="rut">
+            <button type="submit">Buscar reservas del usuario</button>
         </form>
     </div>
+    <br>
+    <div class="box_entregar_ligteblue1">
+        <div id="div_resultados">
+            <h1><b> Resultados de busqueda: </b></h1>
+            <form action="{{route('post_cancha_entregar_resultados')}}" method="POST">
+                @csrf
 
-    <button class="button" onclick="window.location='{{route('usuario_menumoderador')}}' ">Volver menu</button>
+                @if($mostrarResultados == true && $resultados != "")
+                    <!--  <p> {{$resultados}}</p>  -->
+
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Fecha</th>
+                                <th>Nombre</th>
+                                <th>Horario</th>
+                                <th>Capacidad/Cantidad</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                                <tr>
+                                <th> </th>
+                                <th> </th>
+                                <th> </th>
+                                <th> </th>
+                            </tr>
+                            
+                        </tbody>
+                    </table>
+
+
+                <button class="button-register" type="submit">Entregar</button>
+                
+                @else
+                    <p>No se encontraron resultados.</p>
+                @endif
+
+            </form>
+        </div>
+        <button class="button-volver" onclick="window.location='{{route('usuario_menumoderador')}}' ">Volver menu</button>
+        
+
+    
 @endsection
