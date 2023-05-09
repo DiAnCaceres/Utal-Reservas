f198a07ce89ebaa1b9bf1dedc2e9ea5a5b362e32
sctavo <gsgsnchz09@gmail.com>
2023-05-09 14:56:41 -0400

    Avance 1.1
    Creación de los cuadros de CSS y aplicación en la plantilla de recepcionar sala de gimnasio por parte de vista del moderador. (vista principal del requisito)

--- Utal-reservas/resources/views/SalaGimnasio/recepcionar.blade.php
+++ Utal-reservas/resources/views/SalaGimnasio/recepcionar.blade.php
@@ -8,37 +8,67 @@
 @endsection
 
 @section('content')
+
     <div class="botonera">
         <button type="button" class="btn btn-default col-xs-4 boton_servicios" onclick="window.location='{{ route('salaestudio_recepcionar') }}'">Salas de estudio</button>
-        <button type="button" class="btn btn-default col-xs-4 boton_activo">Salas Gimnasio</button>
+        <button type="button" class="btn btn-default col-xs-4 boton_servicios" onclick="window.location='{{ route('salagimnasio_recepcionar') }}'"> Salas gimnasio</button>
 
         <!--
         <button type="button" class="btn btn-default col-xs-4 boton_activo">Implementos</button>
          -->
     </div>
-
-     <h1> Recepcionar sala gimnasio</h1>
-
-
-    <form action="{{route('post_salagimnasio_recepcionar')}}" method="POST">
-        @csrf
-        <button type="submit">Buscar reservas del usuario</button>
-    </form>
-
-    <div id="div_resultados">
-        <h1> Resultados de busqueda </h1>
-        <form action="{{route('post_salagimnasio_recepcionar_resultados')}}" method="POST">
+    
+    <br><br>
+    <div class="box_recepcionar_ligteblue">     
+        <h1><b> Recepcion de sala de gimnasio: </b></h1>
+    
+        <form action="{{route('post_salagimnasio_recepcionar')}}" method="POST">
             @csrf
-
-            @if($mostrarResultados == true && $resultados != "")
-                <p> {{$resultados}}</p>
-                <button type="submit">Entregar</button>
-            @else
-                <p>No se encontraron resultados.</p>
-            @endif
+            <input type="text" placeholder="Rut: 12.345.678-9" name="rut">
+            <button type="submit">Buscar reservas del usuario</button>
         </form>
     </div>
+    <br>
+    <div class="box_recepcionar_ligteblue1">
+        <div id="div_resultados">
+            <h1><b> Resultados de busqueda: </b></h1>
+            <form action="{{route('post_salagimnasio_entregar')}}" method="POST">
+                @csrf
+
+                @if($mostrarResultados == true && $resultados != "")
+                    <!--  <p> {{$resultados}}</p>  -->
+
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Fecha</th>
+                                <th>Nombre</th>
+                                <th>Horario</th>
+                                <th>Capacidad/Cantidad</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                                <tr>
+                                <th> </th>
+                                <th> </th>
+                                <th> </th>
+                                <th> </th>
+                            </tr>
+                            
+                        </tbody>
+                    </table>
 
 
-    <button class="button" onclick="window.location='{{route('usuario_menumoderador')}}' ">Volver menu</button>
+                <button class="button-register" type="submit">Recepcionar</button>
+                
+                @else
+                    <p>No se encontraron resultados.</p>
+                @endif
+
+            </form>
+        </div>
+        <button class="button-volver" onclick="window.location='{{route('usuario_menumoderador')}}' ">Volver menu</button>
+        
+
+    
 @endsection
