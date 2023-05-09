ce15400d85f22108bf5fd6e7e70804b79e9ba7d6
JavierVF <jvillarroel20@alumnos.utalca.cl>
2023-05-09 07:04:28 -0400

    Avance de requisito 2
    se aplico todo lo que deberia ahcer el requisito pero falta dejarlo bonito ademas falta modificar la vista sql de luis para que no muestre las reservas ya seleccionadas

--- Utal-reservas/resources/views/Cancha/entregar.blade.php
+++ Utal-reservas/resources/views/Cancha/entregar.blade.php
@@ -19,58 +19,82 @@
         <button type="button" class="btn btn-default col-xs-4 boton_activo">Implementos</button>
          -->
     </div>
-    
-    <br><br>
-    <div class="box_entregar_ligteblue">     
-        <h1><b> Buscar reserva para entregar cancha: </b></h1>
-    
-        <form action="{{route('post_cancha_entregar')}}" method="POST">
-            @csrf
-            <input type="text" placeholder="Rut: 12.345.678-9" name="rut">
-            <button type="submit">Buscar reservas del usuario</button>
-        </form>
-    </div>
+
     <br>
+
     <div class="box_entregar_ligteblue1">
         <div id="div_resultados">
-            <h1><b> Resultados de busqueda: </b></h1>
+
+            <h1> Entregar cancha</h1>
+
+            <form action="{{route('post_cancha_entregar')}}" method="POST">
+                @csrf
+                <input type="text" placeholder="Rut: 12.345.678-9" name="rut">
+                <button type="submit">Buscar reservas del usuario</button>
+            </form>
+
+            @if (session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+            @endif
+        </div>
+    </div>
+
+    <br>
+
+    <div class="box_entregar_ligteblue1">
+        <div id="div_resultados">
+
+            <h1> Resultados de busqueda </h1>
             <form action="{{route('post_cancha_entregar_resultados')}}" method="POST">
                 @csrf
 
-                @if($mostrarResultados == true && $resultados != "")
-                    <!--  <p> {{$resultados}}</p>  -->
+                @if($mostrarResultados == true)
+
+                    <p>
+                        <b>Nombre:</b> {{ $resultados[0]->name }} <br>
+                        <b>Rut:</b> {{ $resultados[0]->rut }} <br>
+                        <b>Correo:</b> {{ $resultados[0]->email }} <br>
+
+                    </p>
 
                     <table class="table table-striped">
                         <thead>
                             <tr>
                                 <th>Fecha</th>
                                 <th>Nombre</th>
-                                <th>Horario</th>
-                                <th>Capacidad/Cantidad</th>
+                                <th>Hora inicio</th>
+                                <th>Hora fin</th>
+                                <th>Ubicacion </th>
+                                <th>Entregar</th>
                             </tr>
                         </thead>
                         <tbody>
-                                <tr>
-                                <th> </th>
-                                <th> </th>
-                                <th> </th>
-                                <th> </th>
+                            @foreach($resultados as $resultado)
+                            <tr>
+                                <th> {{ $resultado->fecha_reserva }} </th>
+                                <th> {{ $resultado->nombre }} </th>
+                                <th> {{ $resultado->hora_inicio }} </th>
+                                <th> {{ $resultado->hora_fin }} </th>
+                                <th> {{ $resultado->nombre_ubicacion }} </th>
+                                <td>
+                                    <input type="checkbox" name="resultados_seleccionados[]" value="{{ $resultado->fecha_reserva }}|{{ $resultado->reserva_id }}|{{ $resultado->user_id }}|{{ $resultado->bloque_id }}">
+                                </td>
                             </tr>
-                            
+                            @endforeach
                         </tbody>
                     </table>
 
-
-                <button class="button-register" type="submit">Entregar</button>
-                
+                    <button type="submit">Entregar</button>
+                    
                 @else
                     <p>No se encontraron resultados.</p>
                 @endif
 
             </form>
         </div>
-        <button class="button-volver" onclick="window.location='{{route('usuario_menumoderador')}}' ">Volver menu</button>
-        
-
+    </div>
+    <button class="button" onclick="window.location='{{route('usuario_menumoderador')}}' ">Volver menu</button>
     
 @endsection
