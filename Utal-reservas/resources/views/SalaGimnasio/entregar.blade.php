cce39dcdeb984b4fd7f85da9e33f3dc790625dd2
Alonso <aorostica20@alumnos.utalca.cl>
2023-05-10 12:59:26 -0400

    Corrección de diseño
    Se corrigió el diseño de css que se perdió durante los pull request

--- Utal-reservas/resources/views/SalaGimnasio/entregar.blade.php
+++ Utal-reservas/resources/views/SalaGimnasio/entregar.blade.php
@@ -21,74 +21,89 @@
 
     <br>
 
-    <div class="contenedorReserva">
-      <div class="box_reserva_ligteblue">
-        <form action="{{route('post_salagimnasio_entregar')}}" method="POST">
+   
+      <div class="box_entregar_ligteblue">
+        <div id="div_resultados">
+          <h1> Entregar sala de gimnasio</h1>
+          <form action="{{route('post_salagimnasio_entregar')}}" method="POST">
             @csrf
-            <h1> Entregar sala de gimnasio</h1>
-            <div class="rut_usuario">
-                <label for="">Rut</label>
-                <input type="text" placeholder="Rut" name="rut">
+            
+            
+                <input type="text" placeholder="Rut: 12.345.678-9" name="rut">
                 <button class="button" type="submit">Buscar reservas del usuario</button>
                 
-            </div>
             @if ($errors->has('rut'))
                 <span class="text-danger">{{ $errors->first('rut') }}</span>
             @endif
-        </form>
-    
-        <div id="div_resultados">
-            <h1> Resultados de busqueda </h1>
-            <form action="{{route('post_salagimnasio_entregar_resultados')}}" method="POST">
-                @csrf
-    
-                @if($mostrarResultados == true && !empty($resultados))
 
-                <h1 align="left"> Datos del estudiante: </h1>
-                  <p>
-                    <b>Nombre:</b> {{ $resultados[0]->name }} <br>
-                    <b>Rut:</b> {{ $resultados[0]->rut }} <br>
-                    <b>Correo:</b> {{ $resultados[0]->email }} <br>
-                    
-                  </p>
-                <h1 align="left"> Resultados de busqueda: </h1>
+            @if (session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+            @endif
+          </form>
 
-                <table class="tabla_resultados">
-                    <thead>
-                      <tr>
-                        <th>Fecha</th>
-                        <th>Nombre sala</th>
-                        <th>Horario inicio</th>
-                        <th>Horario fin</th>
-                        <th>Capacidad</th>
-                        <th>Entregar</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      @foreach ($resultados as $resultado)
-                        <tr>
-                          <td>{{ $resultado->fecha_reserva }}</td>
-                          <td>{{ $resultado->nombre }}</td>
-                          <td>{{ $resultado->hora_inicio }}</td>
-                          <td>{{ $resultado->hora_fin }}</td>
-                          <td>{{ $resultado->capacidad }}</td>
-                          <td> 
-                            <input type="checkbox" name="resultado[]" value="{{$resultado->fecha_reserva}}, {{$resultado->bloque_id}},{{$resultado->reserva_id}}, {{$resultado->user_id}}">
-                          </td>
-                        </tr>
-                      @endforeach
-                    </tbody>
-                  </table>
-                  
-                <button type="submit">Entregar</button>
-                <button class="button" onclick="window.location='{{route('usuario_menumoderador')}}' ">Volver menu</button>
-                @else
-                    <p>No se encontraron resultados.</p>
-                @endif
-    
-            </form>
-           
         </div>
       </div>
+
+      <br>
+      
+      <div class="box_entregar_ligteblue1">
+
+        <div id="div_resultados">
+          <h1> Resultados de busqueda </h1>
+          <form action="{{route('post_salagimnasio_entregar_resultados')}}" method="POST">
+              @csrf
+  
+              @if($mostrarResultados == true && !empty($resultados))
+
+              <h1 align="left"> Datos del estudiante: </h1>
+                <p>
+                  <b>Nombre:</b> {{ $resultados[0]->name }} <br>
+                  <b>Rut:</b> {{ $resultados[0]->rut }} <br>
+                  <b>Correo:</b> {{ $resultados[0]->email }} <br>
+                  
+                </p>
+              <h1 align="left"> Resultados de busqueda: </h1>
+
+              <table class="tabla_resultados">
+                  <thead>
+                    <tr>
+                      <th>Fecha</th>
+                      <th>Nombre sala</th>
+                      <th>Horario inicio</th>
+                      <th>Horario fin</th>
+                      <th>Capacidad</th>
+                      <th>Entregar</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach ($resultados as $resultado)
+                      <tr>
+                        <td>{{ $resultado->fecha_reserva }}</td>
+                        <td>{{ $resultado->nombre }}</td>
+                        <td>{{ $resultado->hora_inicio }}</td>
+                        <td>{{ $resultado->hora_fin }}</td>
+                        <td>{{ $resultado->capacidad }}</td>
+                        <td> 
+                          <input type="checkbox" name="resultado[]" value="{{$resultado->fecha_reserva}}, {{$resultado->bloque_id}},{{$resultado->reserva_id}}, {{$resultado->user_id}}">
+                        </td>
+                      </tr>
+                    @endforeach
+                  </tbody>
+                </table>
+                
+              <button type="submit">Entregar</button>
+              <button class="button" onclick="window.location='{{route('usuario_menumoderador')}}' ">Volver menu</button>
+              @else
+                  <p>No se encontraron resultados.</p>
+              @endif
+  
+          </form>
+         
+      </div>
     </div>
+
+      </div>
+    
 @endsection
