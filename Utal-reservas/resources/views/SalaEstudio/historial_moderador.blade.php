1f1528ca96b96d3de68357cc731f39ac1fc39f7e
Luis Zamorano Molina <lzamorano20@alumnos.utalca.cl>
2023-05-11 19:04:37 -0400

    VISTAS: Todas las vistas para el historial, tanto del estudiante como moderador

---
+++ Utal-reservas/resources/views/SalaEstudio/historial_moderador.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Utal-Reservas')
+
+@section('estilos')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link rel="stylesheet" href=" {{ asset('css/app.css') }} "/>
+@endsection
+
+@section('content')
+    <h1> Historial Salas de Estudio</h1>
+@endsection
